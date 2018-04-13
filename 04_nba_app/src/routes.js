7035b2058d8ce0cd8f8da889d3b406c0ef1a062d
Brandon <brandon@reddirtwebdesign.com>
2018-04-13 12:54:33 +0000

    About To Eject

--- 04_nba_app/src/routes.js
+++ 04_nba_app/src/routes.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import Home from './components/Home/Home'
+import Layout from './hoc/Layout/Layout'
 
 class Routes extends Component {
   render(){
     return (
       
-      <Switch>
-        <Route path="/" component={Home} exact />  
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" component={Home} exact />  
+        </Switch>
+      </Layout>
     )
   }
 }

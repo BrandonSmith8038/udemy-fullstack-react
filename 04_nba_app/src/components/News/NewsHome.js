f70c6263dc8c725159d53da304d17906e766ba01
Brandon <brandon@reddirtwebdesign.com>
2018-04-15 10:07:29 +0000

    Refactored News Home To SFC

--- 04_nba_app/src/components/News/NewsHome.js
+++ 04_nba_app/src/components/News/NewsHome.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import axios from 'axios'
 import NewsSlider from '../Widgets/NewsSlider/Slider';
 import NewsList from '../Widgets/NewsList/NewsList'
@@ -6,30 +6,26 @@ import NewsList from '../Widgets/NewsList/NewsList'
 import { JSON_SERVER } from '../../config'
 
 
-class NewsHome extends Component {
-  render(){
-    return(
-      <div>
-        <NewsSlider 
-          type="featured"
-          start={0}
-          amount={1}
-          settings = {{
-            dots: false
-          }}
-        />
-        <NewsList
-          type="PicCard"
-          loadmore={true}
-          start={1}
-          amount={5}
-        />
-        
-      </div>
-    )
-  }
-  
-  
+const NewsHome = () => {
+  return(
+    <div>
+      <NewsSlider 
+        type="featured"
+        start={0}
+        amount={1}
+        settings = {{
+          dots: false
+        }}
+      />
+      <NewsList
+        type="PicCard"
+        loadmore={true}
+        start={1}
+        amount={5}
+      />
+      
+    </div>
+  )
 }
 
 export default NewsHome

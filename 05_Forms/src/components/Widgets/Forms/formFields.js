2bf37dd9f8eed457823ddf03c844815795da387e
Brandon <brandon@reddirtwebdesign.com>
2018-04-15 09:43:45 -0700

    Form validation for empty fields

--- 05_Forms/src/components/Widgets/Forms/formFields.js
+++ 05_Forms/src/components/Widgets/Forms/formFields.js
@@ -1,103 +1,115 @@
-import React from 'react'
+import React from "react";
 
-
-
-const FormFields = (props) => {
-  
+const FormFields = props => {
   const renderFields = () => {
-    const formArray = []
-    
-    for(let elementName in props.formData){
+    const formArray = [];
+
+    for (let elementName in props.formData) {
       formArray.push({
         id: elementName,
         settings: props.formData[elementName]
-      })
+      });
     }
     return formArray.map((item, i) => {
       return (
         <div key={i} className="form_element">
           {renderTemplates(item)}
         </div>
-      )
-    })
-  }
-  
+      );
+    });
+  };
+
   const showLabel = (show, label) => {
-    return show ? <label>{label}</label> : null
-  }
-  
+    return show ? <label>{label}</label> : null;
+  };
+
   const changeHandler = (event, id) => {
-    const newState = props.formData
-    
-    newState[id].value = event.target.value
-    props.change(newState)
-  }
-  
-  
-  
+    const newState = props.formData;
+    newState[id].value = event.target.value;
+
+    let validData = validate(newState[id]);
+
+    newState[id].valid = validData[0];
+    newState[id].validationMessage = validData[1];
+
+    props.change(newState);
+  };
+
+  const validate = element => {
+    let error = [true, ""];
+    if (element.validation.required) {
+      const valid = element.value.trim() !== "";
+      const message = `${!valid ? "This field is required" : ""}`;
+
+      error = !valid ? [valid, message] : error;
+    }
+    return error;
+  };
+
+  const showValidationMessage = data => {
+    let errorMessage = null;
+    if (data.validation.required && !data.valid) {
+      return <div className="label_error">{data.validationMessage}</div>;
+    }
+    return errorMessage;
+  };
+
   const renderTemplates = data => {
-    let formTemplate = ''
-    let values = data.settings
+    let formTemplate = "";
+    let values = data.settings;
     switch (values.element) {
-      case 'input':
+      case "input":
         formTemplate = (
           <div>
             {showLabel(values.label, values.labelText)}
-            <input 
+            <input
               {...values.config}
               value={values.value}
-              onChange ={
-                event => changeHandler(event, data.id)
-              }
+              onChange={event => changeHandler(event, data.id)}
             />
+            {showValidationMessage(values)}
           </div>
-        )
+        );
         break;
-      case ('textarea'):
+      case "textarea":
         formTemplate = (
           <div>
             {showLabel(values.label, values.labelText)}
             <textarea
               {...values.config}
               value={values.value}
-              onChange ={
-                event => changeHandler(event, data.id)
-              }
+              onChange={event => changeHandler(event, data.id)}
             />
           </div>
-        )
+        );
         break;
-      case('select'):
+      case "select":
         formTemplate = (
           <div>
             {showLabel(values.label, values.labelText)}
             <select
               value={values.value}
               name={values.config.name}
-              onChange ={
-                event => changeHandler(event, data.id)
-              }
+              onChange={event => changeHandler(event, data.id)}
             >
-              { values.config.options.map((item,i) => {
+              {values.config.options.map((item, i) => {
                 return (
-                  <option key={i} value={item.val}>{item.text}</option>
-                )
-              }) }
+                  <option key={i} value={item.val}>
+                    {item.text}
+                  </option>
+                );
+              })}
             </select>
           </div>
-        )
+        );
         break;
       default:
-       formTemplate = null
+        formTemplate = null;
     }
-    return formTemplate
-  }
-  
-  return (
-    <div>
-      {renderFields()}
-    </div>
-  )
-}
+    return formTemplate;
+  };
 
-export default FormFields
+  return <div>{renderFields()}</div>;
+};
+
+export default FormFields;
--- 05_Forms/src/components/user.js
+++ 05_Forms/src/components/user.js
@@ -1,94 +1,116 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import FormFields from './Widgets/Forms/formFields'
+import FormFields from "./Widgets/Forms/formFields";
 
 class User extends Component {
-
-    state = {
-        formData: {
-            name: {
-                element: 'input',
-                value: '',
-                label: true,
-                labelText: 'Name',
-                config: {
-                    name: 'name_input',
-                    type: 'text',
-                    placeholder: 'Enter Your name'
-                }
-            },
-            lastname: {
-                element: 'input',
-                value: '',
-                label: true,
-                labelText: 'Last Name',
-                config: {
-                    name: 'lastname_input',
-                    type: 'text',
-                    placeholder: 'Enter Your lastname'
-                }
-            },
-            message: {
-               element: 'textarea',
-                value: '',
-                label: true,
-                labelText: 'Message',
-                config: {
-                    name: 'message_input',
-                    rows: 3,
-                    cols: 36
-                } 
-            },
-            age: {
-               element: 'select',
-                value: '',
-                label: true,
-                labelText: 'Message',
-                config: {
-                    name: 'age_input',
-                    rows: 3,
-                    options: [
-                        {val:'1', text:'10-20'},    
-                        {val:'2', text:'20-30'},   
-                        {val:'3', text:'30-40'},  
-                    ]
-                } 
-            }
+  state = {
+    formData: {
+      name: {
+        element: "input",
+        value: "",
+        label: true,
+        labelText: "Name",
+        config: {
+          name: "name_input",
+          type: "text",
+          placeholder: "Enter Your name"
+        },
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false,
+        validationMessage: ""
+      },
+      lastname: {
+        element: "input",
+        value: "",
+        label: true,
+        labelText: "Last Name",
+        config: {
+          name: "lastname_input",
+          type: "text",
+          placeholder: "Enter Your lastname"
+        },
+        validation: {
+          required: true,
+          valid: false,
+          touched: false,
+          validationMessage: ""
         }
-        
-    }
-    
-    updateForm = newState => {
-        this.setState({
-            formData: newState
-        })
-    }
-    
-    submitForm = event => {
-        event.preventDefault()
-    
-        let dataToSubmit = {}
-    
-        for(let key in this.state.formData){
-            dataToSubmit[key] = this.state.formData[key].value
+      },
+      message: {
+        element: "textarea",
+        value: "",
+        label: true,
+        labelText: "Message",
+        config: {
+          name: "message_input",
+          rows: 3,
+          cols: 36
+        },
+        validation: {
+          required: false,
+          valid: false,
+          touched: false,
+          validationMessage: ""
         }
-        console.log(dataToSubmit)
+      },
+      age: {
+        element: "select",
+        value: "",
+        label: true,
+        labelText: "Message",
+        config: {
+          name: "age_input",
+          rows: 3,
+          options: [
+            { val: "1", text: "10-20" },
+            { val: "2", text: "20-30" },
+            { val: "3", text: "30-40" }
+          ]
+        },
+        validation: {
+          required: false,
+          valid: false,
+          touched: false,
+          validationMessage: ""
+        }
+      }
     }
+  };
 
-    render(){
-        return(
-            <div className="container">
-                User
-                <form onSubmit={this.submitForm}>
-                    <FormFields 
-                        formData={this.state.formData}
-                        change={(newState) => this.updateForm(newState)}
-                    />
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
-        )
+  updateForm = newState => {
+    this.setState({
+      formData: newState
+    });
+  };
+
+  submitForm = event => {
+    event.preventDefault();
+
+    let dataToSubmit = {};
+
+    for (let key in this.state.formData) {
+      dataToSubmit[key] = this.state.formData[key].value;
     }
+    console.log(dataToSubmit);
+  };
+
+  render() {
+    return (
+      <div className="container">
+        User
+        <form onSubmit={this.submitForm}>
+          <FormFields
+            formData={this.state.formData}
+            change={newState => this.updateForm(newState)}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default User;
